899ae1900d611e57ec497c785f9e16f6ce332dd7
eshernan@gmail.com <eshernan@gmail.com>
2023-05-11 11:33:44 -0500

    add mpi compiling support

--- compiling.sh
+++ compiling.sh
@@ -6,3 +6,6 @@ gcc -o matmul_serial matmul_serial.c
 gcc -fopenmp -o matmul_parallel matmul_parallel.c
 
 echo "Compile Finished ....." 
+
+# mpicc parallel compiling with openmpi
+mpicc -fopenmp mix_matrix.c -o mix_matrix
